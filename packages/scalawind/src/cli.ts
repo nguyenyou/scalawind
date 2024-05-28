4b9c27935b690cf65540a0dee301537b88e59da2
nguyenyou <tunt081295@gmail.com>
2024-05-28 10:19:36 +0700

    remove unused code

--- packages/scalawind/src/cli.ts
+++ packages/scalawind/src/cli.ts
@@ -2,7 +2,6 @@
 
 import fs from 'fs';
 import path from 'path';
-import { transform } from 'lightningcss';
 import { createScalawindContext, loadConfig } from './utils';
 
 import Handlebars from "handlebars";
@@ -11,56 +10,7 @@ const scalawindTemplate = fs.readFileSync(path.join(__dirname, "./templates/scal
 
 const template = Handlebars.compile(scalawindTemplate);
 
-function createDoc(doc: string) {
-  try {
-    let cssDoc = `
-    * \`\`\`css
-    * ${transform({
-      filename: 'doc.css',
-      code: Buffer.from(doc),
-    })
-      .code.toString()
-      .replace(/\n/g, '\n    * ')}
-    * \`\`\`
-  `;
-    const config = loadConfig();
-    if (config.showPixelEquivalents) {
-      const remMatch = doc.match(/-?[0-9.]+rem/g);
-      const pxValue = config.rootFontSize;
-      if (remMatch) {
-        cssDoc = cssDoc.replace(
-          /(-?[0-9.]+)rem/g,
-          // There is a zero-width space between * and / in the closing comment
-          // without which typescript closes the tsdoc comment
-          (match, p1) => `${match} /* ${parseFloat(p1) * pxValue}px *​/`
-        );
-      }
-    }
-    return cssDoc;
-  } catch (error) {
-    return doc;
-  }
-}
-
 const fmtToScalawind = (s: string) => s.replace(/-/g, '_').replace(/^\@/, '$').replace(/%/, '');
-
-const objectTemplate = (
-  props: { prop: string; type: string; doc?: string }[]
-) => {
-  return `{${props
-    .map(
-      ({ prop, type, doc }) =>
-        `/** ${doc ? createDoc(doc) : ''} */ ${JSON.stringify(prop)}: ${type};`
-    )
-    .join('\n')}}`;
-};
-
-const typeTemplate = (
-  name: string,
-  props: { prop: string; type: string; doc?: string }[]
-) => `
-type ${name} = ${objectTemplate(props)}
-`;
 
 function getCandidateItem(
   map: Map<string, any>,
@@ -87,7 +37,6 @@ export async function generateTypes() {
 
   const classList = ctx.getClassList() as string[];
 
-  const opacityMap = ctx.tailwindConfig.theme.opacity;
   const flatColorsList: string[] = [];
 
   for (const [k, v] of Object.entries(ctx.tailwindConfig.theme.colors)) {
@@ -139,44 +88,6 @@ export async function generateTypes() {
     return { prop: fmtToScalawind(s), raw: s, type: 'Property', doc: css };
   })
   
-  const candidates = [...ctx.candidateRuleMap.entries()];
-  const arbitraryStyles = [];
-  for (const [name, rules] of candidates) {
-    const ident = fmtToScalawind(name) + '_';
-    const styles: string[] = [];
-
-    for (const [rule, fn] of rules) {
-      if (
-        !rule.options ||
-        !rule.options.values ||
-        Object.keys(rule.options.values).length == 0
-      )
-        continue;
-
-      styles.push(
-        objectTemplate(
-          Object.keys(rule.options.values).map((val) => {
-            const [ruleSet] = fn(val, {});
-
-            return {
-              prop: val,
-              type: 'Property',
-              doc: fmtRuleToCss(ruleSet),
-            };
-          })
-        )
-      );
-    }
-
-    arbitraryStyles.push({
-      prop: ident,
-      type: styles.join(' & ') + ' & Record<string, Property>',
-      doc: undefined,
-    });
-  }
-
-  const arbitrary = typeTemplate('Arbitrary', arbitraryStyles);
-
   const modifiers = [...ctx.variantMap.keys()]
   // Remove * from the list of modifiers to avoid syntax error
   .filter((s) => s !== '*')
