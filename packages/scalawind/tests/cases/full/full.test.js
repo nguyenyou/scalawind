6d789a10fa13db56f921f5ab7a1e5b5b0bda3874
nguyenyou <tunt081295@gmail.com>
2024-06-02 10:05:01 +0700

    update snapshot test

--- packages/scalawind/tests/cases/full/full.test.js
+++ packages/scalawind/tests/cases/full/full.test.js
@@ -3,11 +3,9 @@ import fs from 'fs'
 import path from 'path'
 import { generateContent } from '../../../src/generate'
 import { tailwindConfig } from './config'
-import { debugActual } from '../../test-utils'
 
 test('full config test', () => {
   const actual = generateContent(tailwindConfig, "scalawind")
-  debugActual(actual)
   const expected = fs.readFileSync(path.join(__dirname, "./expected.txt"), "utf8")
-  expect(true).toBe(true)
+  expect(actual).toBe(expected)
 })
