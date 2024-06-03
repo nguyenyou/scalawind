ecbb9cd677fc7269cfafff0afa6f40ee19f4a79f
Tu Nguyen <tunt081295@gmail.com>
2024-06-03 10:44:17 +0700

    more tests (#89)

--- integration/src/scalawind.test.scala
+++ integration/src/scalawind.test.scala
@@ -2,14 +2,38 @@
 
 import scalawind.*
 
-class ScalawindTests extends munit.FunSuite {
+class SimpleSingleTests extends munit.FunSuite {
   test("normal") {
     assert(sw(tw.flex.items_center.justify_center) == "flex items-center justify-center")
   }
   test("modifier") {
-    assert(sw(tw.md(tw.hover(tw.text_red_500))) == "md:hover:text-red-500")
+    assert(sw(tw.hover(tw.text_red_500)) == "hover:text-red-500")
   }
   test("arbitrary values") {
     assert(sw(tw.text_("#ff0")) == "text-[#ff0]")
+  }
+  test("opacity color") {
+    assert(sw(tw.bg_red_400.opacity(10)) == "bg-red-400/10")
+  }
+  test("dot values") {
+    assert(sw(tw.w_1dot5) == "w-1.5")
+  }
+  test("percentage values") {
+    assert(sw(tw.w_1$2) == "w-1/2")
+  }
+  test("negative values") {
+    assert(sw(tw._left_1) == "-left-1")
+  }
+  test("raw values") {
+    assert(sw(tw.raw("text-green-400")) == "text-green-400")
+  }
+}
+
+class ComplexChainingTests extends munit.FunSuite {
+  test("responsive") {
+    assert(sw(tw.md(tw.hover(tw.text_red_500))) == "md:hover:text-red-500")
+  }
+  test("arbitrary values") {
+    assert(sw(tw.flex.text_("#ff0").items_center.bg_("#00f")) == "flex text-[#ff0] items-center bg-[#00f]")
   }
 }
