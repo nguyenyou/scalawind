55b62691a3c20ab1069453efefb8c19e484070d1
nguyenyou <tunt081295@gmail.com>
2024-06-03 14:12:27 +0700

    more tests and format tests

--- integration/src/scalawind.test.scala
+++ integration/src/scalawind.test.scala
@@ -4,32 +4,72 @@ import scalawind.*
 
 class SimpleSingleTests extends munit.FunSuite {
   test("normal") {
-    assert(sw(tw.flex.items_center.justify_center) == "flex items-center justify-center")
-    assert(sw(tw.bg_blue_500.text_white.rounded.py_3.px_4) == "bg-blue-500 text-white rounded py-3 px-4")
+    assert(
+      sw(
+        tw.flex.items_center.justify_center
+      ) == "flex items-center justify-center"
+    )
+    assert(
+      sw(
+        tw.bg_blue_500.text_white.rounded.py_3.px_4
+      ) == "bg-blue-500 text-white rounded py-3 px-4"
+    )
   }
   test("modifier") {
-    assert(sw(tw.hover(tw.text_red_500)) == "hover:text-red-500")
+    assert(
+      sw(
+        tw.hover(tw.text_red_500)
+      ) == "hover:text-red-500"
+    )
   }
   test("arbitrary values") {
-    assert(sw(tw.text_("#ff0")) == "text-[#ff0]")
+    assert(
+      sw(
+        tw.text_("#ff0")
+      ) == "text-[#ff0]"
+    )
   }
   test("opacity color") {
-    assert(sw(tw.bg_red_400.opacity(10)) == "bg-red-400/10")
+    assert(
+      sw(
+        tw.bg_red_400.opacity(10)
+      ) == "bg-red-400/10"
+    )
   }
   test("dot values") {
-    assert(sw(tw.w_1dot5) == "w-1.5")
+    assert(
+      sw(
+        tw.w_1dot5
+      ) == "w-1.5"
+    )
   }
   test("percentage values") {
-    assert(sw(tw.w_1$2) == "w-1/2")
+    assert(
+      sw(
+        tw.w_1$2
+      ) == "w-1/2"
+    )
   }
   test("negative values") {
-    assert(sw(tw._left_1) == "-left-1")
+    assert(
+      sw(
+        tw._left_1
+      ) == "-left-1"
+    )
   }
   test("raw values") {
-    assert(sw(tw.raw("text-green-400")) == "text-green-400")
+    assert(
+      sw(
+        tw.raw("text-green-400")
+      ) == "text-green-400"
+    )
   }
   test("important modifier") {
-    assert(sw(tw.important(tw.text_red_400)) == "!text-red-400")
+    assert(
+      sw(
+        tw.important(tw.text_red_400)
+      ) == "!text-red-400"
+    )
   }
 }
 
@@ -51,12 +91,39 @@ class ComplexChainingTests extends munit.FunSuite {
     )
   }
   test("arbitrary values") {
-    assert(sw(tw.flex.text_("#ff0").items_center.bg_("#00f")) == "flex text-[#ff0] items-center bg-[#00f]")
+    assert(
+      sw(
+        tw.flex.text_("#ff0").items_center.bg_("#00f")
+      ) == "flex text-[#ff0] items-center bg-[#00f]"
+    )
+    assert(
+      sw(
+        tw.text_("20px").py_3.px_4.bg_blue_500
+      ) == "text-[20px] py-3 px-4 bg-blue-500"
+    )
   }
   test("important modifer") {
-    assert(sw(tw.sm(tw.hover(tw.important(tw.font_bold)))) == "sm:hover:!font-bold")
+    assert(
+      sw(
+        tw.sm(tw.hover(tw.important(tw.font_bold)))
+      ) == "sm:hover:!font-bold"
+    )
+    assert(
+      sw(
+        tw.important(tw.text_red_500).hover(tw.important(tw.text_red_600))
+      ) == "!text-red-500 hover:!text-red-600"
+    )
   }
   test("arbitrary variant") {
-    assert(sw(tw.lg(tw.variant("&:nth-child(3)", tw.hover(tw.underline)))) == "lg:[&:nth-child(3)]:hover:underline")
+    assert(
+      sw(
+        tw.lg(tw.variant("&:nth-child(3)", tw.hover(tw.underline)))
+      ) == "lg:[&:nth-child(3)]:hover:underline"
+    )
+    assert(
+      sw(
+        tw.variant("&:nth-child(3)", tw.underline).list_disc.mx_5.text_white
+      ) == "[&:nth-child(3)]:underline list-disc mx-5 text-white"
+    )
   }
 }
