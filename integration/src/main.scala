56118fd699bacb1de4e213b20e48604b48a6407b
Tu Nguyen <tunt081295@gmail.com>
2024-06-05 17:04:55 +0700

    add create-next-app homepage test (#110)

--- integration/src/main.scala
+++ integration/src/main.scala
@@ -1,6 +1,16 @@
 import scalawind.*
 
 @main def integration() = {
-  val styles: String = sw(tw.flex.text_("#ff0").items_center.bg_("#00f"))
+  val styles: String = sw(
+    tw.rounded_full.border.border_solid.border_transparent.transition_colors.flex.items_center.justify_center.bg_foreground.text_background.gap_2
+      .hover(tw.bg_("#383838"))
+      .dark(tw.hover(tw.bg_("#333")))
+      .text_sm
+      .sm(tw.text_base)
+      .h_10
+      .sm(tw.h_12)
+      .px_4
+      .sm(tw.px_5)
+  )
   println(styles)
 }
--- integration/tailwind.config.cjs
+++ integration/tailwind.config.cjs
@@ -1,4 +1,18 @@
-module.export = {
-  content: [],
-  theme: {}
-}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    files: ['./index.html', './scalajs-modules/**/*.js'],
+  },
+  theme: {
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+      fontFamily: {
+        sans: ["Inter"],
+        mono: ["Roboto Mono"],
+      },
+    },
+  },
+};
