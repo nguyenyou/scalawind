b28c777c11caa4e0bb21fb027d9301e8fe0c1431
nguyenyou <tunt081295@gmail.com>
2024-06-05 22:22:50 +0700

    update examples

--- examples/vite-slinky/src/index.scala
+++ examples/vite-slinky/src/index.scala
@@ -4,22 +4,43 @@ import org.scalajs.dom
 
 import scalawind.*
 
-import slinky.core._
+import slinky.core.*
+import slinky.core.facade.Hooks.*
 import slinky.web.ReactDOM
-import slinky.web.html._
+import slinky.web.html.*
+
+val Counter = FunctionalComponent[Int] { props =>
+  val (count, setCount) = useState(props)
+  div(
+    className := tw.h_screen.w_screen.flex.items_center.justify_center,
+    button(
+      className := tw.bg_slate_900
+        .hover(tw.bg_slate_700)
+        .focus(
+          tw.outline_none.ring_2.ring_slate_400.ring_offset_2.ring_offset_slate_50
+        )
+        .text_white
+        .font_semibold
+        .h_12
+        .px_6
+        .rounded_lg
+        .w_full
+        .flex
+        .items_center
+        .justify_center
+        .sm(tw.w_auto)
+        .dark(tw.bg_sky_500.hover(tw.bg_sky_400)),
+      s"Counter: ${count}",
+      onClick := (e => {
+        val newCount = count + 1
+        setCount(newCount)
+      })
+    )
+  )
+}
+
 @main
 def App(): Unit =
   val container = dom.document.getElementById("app")
 
-  ReactDOM.render(
-    div(
-      className := sw(tw.w_screen.h_screen.flex.items_center.justify_center),
-      div(
-        "Scala ❤️ Tailwind",
-        className := sw(tw.p_2.text_primary.hover(tw.bg_black.text_white))
-      )
-    ),
-    container
-  )
-
-end App
+  ReactDOM.render(Counter(2024), container)
--- examples/vite-slinky/tailwind.config.cjs
+++ examples/vite-slinky/tailwind.config.cjs
@@ -1,33 +1,7 @@
-const colors = require("tailwindcss/colors");
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: {
     files: ['./index.html', './scalajs-modules/**/*.js'],
   },
-  theme: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      black: colors.black,
-      white: colors.white,
-      red: colors.red,
-    },
-    extend: {
-      colors: {
-        primary: '#DE3423',
-        secondary: ' #380d09'
-      },
-    },
-  },
-  corePlugins: [
-    'display',
-    'textColor',
-    'width',
-    'height',
-    'alignItems',
-    'justifyContent',
-    'padding',
-    'backgroundColor'
-  ]
+  theme: {},
 };
