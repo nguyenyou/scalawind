bbfe4553c696eaf0f4f1f90d9693e02cc48d2169
nguyenyou <tunt081295@gmail.com>
2025-05-07 20:16:30 +0700

    🚢 ship it

--- website/vite.config.js
+++ website/vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+
+const IS_CI = process.env.CI === "true";
+
 export default defineConfig({
   plugins: [tailwindcss()],
+  base: IS_CI ? "/scalawind/" : "/",
 });
